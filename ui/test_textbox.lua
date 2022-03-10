fe9ce8dcd429eb29cf5bf73da0d2609fe799bba8
Daniel Ritter <head@daniel-ritter.biz>
2022-03-11 00:24:44 +0100

    Fix

--- ui/test_textbox.lua
+++ ui/test_textbox.lua
@@ -4,13 +4,17 @@ local TextBox = require "textbox"
 local monitor = peripheral.find("monitor")
 monitor.setBackgroundColor(colors.black)
 monitor.clear()
-local textBox = TextBox:new(monitor, 1, 2, 10)
+local width, _ = monitor.getSize()
+local textBox = TextBox:new(monitor, 1, 2, width)
+textBox.textColor = colors.yellow
+textBox:setText("TestTestTest")
 textBox:setText("Test")
 
-textBox = TextBox:new(monitor, 1, 4, 10)
-textBox.orientation = textBox.CENTER
+textBox = TextBox:new(monitor, 1, 4, width)
+textBox.orientation = textBox.Orientation.CENTER
+textBox.backgroundColor(colors.red)
 textBox:setText("Test")
 
-textBox = TextBox:new(monitor, 1, 6, 10)
-textBox.orientation = textBox.RIGHT
+textBox = TextBox:new(monitor, 1, 6, width)
+textBox.orientation = textBox.Orientation.RIGHT
 textBox:setText("Test")
--- ui/textbox.lua
+++ ui/textbox.lua
@@ -1,29 +1,16 @@
-local Orientation = {
-    LEFT = 0,
-    CENTER = 1,
-    RIGHT = 2
-}
 
 local TextBox = {
+    Orientation = {
+        LEFT = 0,
+        CENTER = 1,
+        RIGHT = 2
+    },
     backgroundColor = colors.black,
     textColor = colors.white,
-    orientation = Orientation.LEFT,
+    orientation = 0,
     _text = "",
     _textLength = 0
 }
-
-local function dump(o)
-    if type(o) == 'table' then
-        local s = '{ '
-        for k,v in pairs(o) do
-            if type(k) ~= 'number' then k = '"'..k..'"' end
-            s = s .. '['..k..'] = ' .. dump(v) .. ','
-        end
-        return s .. '} '
-    else
-        return tostring(o)
-    end
-end
 
 function TextBox:new(monitor, xStart, yStart, width)
     local o = {}
@@ -47,7 +34,6 @@ function TextBox:_paint()
     -- local newTextLength = string.len(self._text)
     -- if newTextLength < self._textLength then
     --     self.monitor.setCursorPos(self.x + newTextLength, self.y)
-    --     print("P " .. "write space at x:" .. (self.x + newTextLength) .. " for " .. (self._textLength - newTextLength) .. " chars")
     --     for _ = 1, self._textLength - newTextLength do
     --         self.monitor.write(" ")
     --     end
@@ -55,7 +41,6 @@ function TextBox:_paint()
     -- end
     self.monitor.setCursorPos(self.x, self.y)
     self.monitor.write(self._text)
-    print("P x:" .. self.x .. " y:" .. self.y .. " t:'" .. self._text .. "'")
     self.monitor.setBackgroundColor(currentBc)
     self.monitor.setTextColor(currentTc)
 end
@@ -64,9 +49,9 @@ function TextBox:_justify(text)
     local justifiedText = ""
     local textLength = string.len(text)
     if textLength < self.width then
-        if self.orientation == Orientation.RIGHT then
+        if self.orientation == self.Orientation.RIGHT then
             justifiedText = string.format(self._rightFormat, text)
-        elseif self.orientation == Orientation.CENTER then
+        elseif self.orientation == self.Orientation.CENTER then
             local remainingSpace = self.width - textLength
             local leftPadding = math.floor(remainingSpace / 2)
             for _ = 1, leftPadding do
@@ -90,7 +75,6 @@ function TextBox:setText(text)
         trimmedText = string.sub(text, 1, tEnd)
     end
     self._text = self:_justify(trimmedText)
-    print(self._text)
     self:_paint()
 end
 
